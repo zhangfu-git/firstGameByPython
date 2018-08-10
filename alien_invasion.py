b65bf3cfa71bff444746aac9b71d4593138f3d98
zhangfu <zhangfu@nasetech.com>
2018-08-10 22:01:28 +0800

    feat: init game

---
+++ alien_invasion.py
@@ -0,0 +1,56 @@
+import pygame
+
+from settings import Settings
+from game_status import GameStats
+from ship import Ship
+from pygame.sprite import Group
+import game_functions as gf
+from button import Button
+from scoreboard import Scoreboard
+
+def run_game():
+  # 初始化游戏并创建一个屏幕对象
+  pygame.init()
+  
+  ai_settings = Settings()
+
+  screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+  
+  pygame.display.set_caption("小郑打苍蝇")
+
+  # 创建Play按钮
+  play_button = Button(ai_settings, screen, "Play")
+
+  # 创建一艘飞船
+  ship = Ship(ai_settings, screen)
+
+  # 创建一个用于存储子弹的编组
+  bullets = Group()
+
+  # 创建外星人群
+  aliens = Group()
+
+  gf.create_fleet(ai_settings, screen, ship, aliens)
+
+  # 创建一个用于存储游戏统计信息的实例
+  stats = GameStats(ai_settings)
+
+  sb = Scoreboard(ai_settings, screen, stats)
+
+  # 开始游戏的主循环
+  while True:
+
+    # 监视键盘和鼠标事件
+    gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+
+    if stats.game_active:
+
+      ship.update()
+
+      gf.update_bullets(ai_settings, screen,stats, sb, ship, aliens, bullets)
+
+      gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+
+    gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+run_game()
